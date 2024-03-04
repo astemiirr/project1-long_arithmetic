2ba1174d24a20d986d48607f948abea3e3fa701b
astemiirr <158389198+astemiirr@users.noreply.github.com>
2024-03-04 12:15:39 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include "big_real.hpp"
 
@@ -11,21 +10,21 @@ int main()
     cin >> s1 >> s2;
     big_real a(s1), b(s2);
 
-    cout << "a: ";
+    cout << "a = ";
     a.print();
     cout << endl;
-    cout << "b: ";
+    cout << "b = ";
     b.print();
-    cout << endl;
+    cout << endl << endl;
 
     big_real c;
     c = a + b;
-    cout << "c = a + b: ";
+    cout << "c = a + b = ";
     c.print();
     cout << "\n";
 
     c = a - b;
-    cout << "c = a - b: ";
+    cout << "c = a - b =  ";
     c.print();
     cout << "\n";
 
