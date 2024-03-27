f7dbef39687be2524e5c180855015b8f815c9098
astemiirr <158389198+astemiirr@users.noreply.github.com>
2024-03-27 21:24:29 +0300

    Update big_real.hpp

--- big_real.hpp
+++ big_real.hpp
@@ -1,20 +1,20 @@
 //big_real.hpp
+#pragma once
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <iomanip>
 
 class big_real {
-	const int DivDigits = 1000;
-
 	int sign;
-	std::vector<int> digits;
-	int dot;
+	std::vector <int> digits;
+	int exponent;
 
-	void remove_extra_zeroes();
+	void removeZeroes();
 	void normalize();
-	std::string tostring();
+
+	const int divDigits = 200;
 
 public:
 	big_real();
@@ -23,19 +23,35 @@ public:
 	big_real(const std::string& s_inp);
 
 	big_real& operator=(const big_real& other);
-	big_real operator-() const;
 
-	bool operator==(const big_real& other) const;
-	bool operator!=(const big_real& other) const;
 	bool operator>(const big_real& other) const;
 	bool operator<(const big_real& other) const;
+	bool operator>=(const big_real& other) const;
+	bool operator<=(const big_real& other) const;
+	bool operator==(const big_real& other) const;
+	bool operator!=(const big_real& other) const;
+
+	big_real operator-() const;
 
 	big_real operator+(const big_real& other) const;
 	big_real operator-(const big_real& other) const;
 	big_real operator*(const big_real& other) const;
 	big_real operator/(const big_real& other) const;
 
+	big_real& operator+=(const big_real& other);
+	big_real& operator-=(const big_real& other);
+	big_real& operator*=(const big_real& other);
+	big_real& operator/=(const big_real& other);
+
+	big_real& operator++();
+	big_real& operator--();
+
 	big_real inverse() const;
+	std::string tostring() const;
+	int fractional_size() const;
+	void pi_cut(int value);
 
 	bool isZero() const;
+
+	friend std::ostream& operator<<(std::ostream& outs, const big_real& value);
 };
