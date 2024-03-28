e01d23d3452f61d0cdf52c12bda3571b8f2dc579
astemiirr <158389198+astemiirr@users.noreply.github.com>
2024-03-28 20:29:46 +0300

    Create pi_calculation.hpp

---
+++ pi_calculation.hpp
@@ -0,0 +1,16 @@
+//pi_calculation.hpp
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cmath>
+#include "big_real.hpp"
+
+namespace mynamespace{
+
+big_real spec_sqrt(big_real n, big_real one);
+
+big_real pi_chudnovsky(int count);
+
+}
