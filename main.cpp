4829c08d17504b4e90902f454397dbb0ec96a02b
astemiirr <158389198+astemiirr@users.noreply.github.com>
2024-03-28 20:28:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,29 @@
 #include <string>
 #include <vector>
 #include <ctime>
-#include "big_real.hpp"
-#include "test.cpp"
-//#include "pi_calculation.cpp"
 
-using namespace std;
+#include "big_real.hpp"
+#include "test.hpp"
+#include "pi_calculation.hpp"
+
+using namespace mynamespace;
 
 int main() {
-	addTest();
-	subTest();
-	multTest();
-	divTest();
+	// addTest();
+	// subTest();
+	// multTest();
+	// divTest();
+
+	int prec;
+    std::cout << "prec = ";
+    std::cin >> prec;
+
+    clock_t start_time = clock();
+    big_real pi = pi_chudnovsky(prec);
+    std::cout << "Pi with " << prec << " signs after the dot: " << pi << std::endl;
+    clock_t finish_time = clock();
+
+    std::cout << "Execution time: " << double(finish_time - start_time) / CLOCKS_PER_SEC << "sec" << std::endl;
 
 	return 0;
 }
